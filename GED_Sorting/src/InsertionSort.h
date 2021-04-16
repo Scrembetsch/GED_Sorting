6b446c77e9e55167f2330c64694ecf3378e45714
Irena Ankerl <gs20m002@technikum-wien.at>
2021-04-16 16:54:15 +0200

    implemented insertion sort

--- GED_Sorting/src/InsertionSort.h
+++ GED_Sorting/src/InsertionSort.h
@@ -3,7 +3,17 @@
 namespace InsertionSort
 {
     template<typename T>
-    void Sort(T*, size_t)
+    void Sort(T* arr, size_t size)
     {
+        size_t curr;
+        for (int i = 0; i < size - 1; i++)
+        {
+            curr = i + 1;
+            while (curr > 0 && arr[curr] < arr[curr - 1])
+            {
+                std::swap(arr[curr], arr[curr - 1]);
+                curr--;
+            }
+        }
     }
 }
