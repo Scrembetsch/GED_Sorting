500277f4b2fe419fc53cf553e4cc3c980b69a7f4
Maximilian Bauer <gs20m003@technikum-wien.at>
2021-04-17 18:59:18 +0200

    Bucket Sort Update

--- GED_Sorting/src/Sort.h
+++ GED_Sorting/src/Sort.h
@@ -122,7 +122,7 @@ namespace BucketSort
 
         // 4) Concatenate all buckets into arr[]
         size_t index = 0;
-        for (size_t i = 0; i < n; i++)
+        for (size_t i = 0; i < n+1; i++)
             for (size_t j = 0; j < b[i].size(); j++)
                 arr[index++] = b[i][j];
 
